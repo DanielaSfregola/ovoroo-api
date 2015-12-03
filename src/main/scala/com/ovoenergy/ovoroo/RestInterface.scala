1fe63466402ba2a13346065e82b6baf83d47f656
Daniela Sfregola <daniela.sfregola@gmail.com>
2015-12-03 19:56:10 +0000

    Second try

--- src/main/scala/com/ovoenergy/ovoroo/RestInterface.scala
+++ src/main/scala/com/ovoenergy/ovoroo/RestInterface.scala
@@ -3,26 +3,51 @@ package com.ovoenergy.ovoroo
 import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
 
-import spray.http.HttpHeaders.RawHeader
+import spray.http._
 import spray.routing._
 
 import com.ovoenergy.ovoroo.resources.{OrderResource, PingResource}
 import com.ovoenergy.ovoroo.services.OrderService
 
-class RestInterface(implicit val executionContext: ExecutionContext) extends HttpServiceActor with Resources {
+class RestInterface(implicit val executionContext: ExecutionContext) extends HttpServiceActor with Resources with CORSSupport {
 
   def receive = runRoute(routes)
 
   val orderService = new OrderService
 
   val routes: Route =
-    respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")) {
-      pingRoutes ~
-      //authenticate(???) { authenticated =>
-      questionRoutes
-      //}
+    cors {
+        pingRoutes ~
+        //authenticate(???) { authenticated =>
+        questionRoutes
+        //}
     }
-
 }
 
 trait Resources extends OrderResource with PingResource
+
+trait CORSSupport {
+  this: HttpService =>
+
+  private val allowOriginHeader = HttpHeaders.`Access-Control-Allow-Origin`(AllOrigins)
+  private val optionsCorsHeaders = List(
+    HttpHeaders.`Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"),
+    HttpHeaders.`Access-Control-Max-Age`(1728000))
+
+  def cors[T]: Directive0 = mapRequestContext { ctx => ctx.withRouteResponseHandling({
+    //It is an option requeset for a resource that responds to some other method
+    case Rejected(x) if (ctx.request.method.equals(HttpMethods.OPTIONS) && !x.filter(_.isInstanceOf[MethodRejection]).isEmpty) => {
+      val allowedMethods: List[HttpMethod] = x.filter(_.isInstanceOf[MethodRejection]).map(rejection=> {
+        rejection.asInstanceOf[MethodRejection].supported
+      })
+      ctx.complete(HttpResponse().withHeaders(
+        HttpHeaders.`Access-Control-Allow-Methods`(HttpMethods.OPTIONS, allowedMethods :_*) ::  allowOriginHeader ::
+        optionsCorsHeaders
+      ))
+    }
+  }).withHttpResponseHeadersMapped { headers =>
+    allowOriginHeader :: headers
+  }
+
+  }
+}
--- src/main/scala/com/ovoenergy/ovoroo/routing/Authentication.scala
+++ src/main/scala/com/ovoenergy/ovoroo/routing/Authentication.scala
@@ -1,22 +1,15 @@
 package com.ovoenergy.ovoroo.routing
 
-import scala.concurrent.Future
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import spray.routing.authentication._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-
 trait Authentication {
 
 
-  def ldapAuthenticator(userPass: Option[UserPass]): Future[Option[String]] =
+/*  def ldapAuthenticator(userPass: Option[UserPass]): Future[Option[String]] =
     Future {
       userPass.map { credentials =>
         val token = new UsernamePasswordAuthenticationToken(credentials.user, credentials.pass)
-        //new AuthenticationProvider().authenticate(token)
+//        new AuthenticationProvider().authenticate(token)
       }
       if (userPass.exists(up => up.user == "John" && up.pass == "p4ssw0rd")) Some("John")
       else None
-    }
+    }*/
 }
