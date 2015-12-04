65f880cd3af655917114ab87e012ef8978931bf9
Daniela Sfregola <daniela.sfregola@gmail.com>
2015-12-04 01:06:12 +0000

    Now returning html directory after authentication

--- src/main/scala/com/ovoenergy/ovoroo/AppInterface.scala
+++ src/main/scala/com/ovoenergy/ovoroo/AppInterface.scala
@@ -3,6 +3,7 @@ package com.ovoenergy.ovoroo
 import scala.concurrent.ExecutionContext
 import scala.language.postfixOps
 
+import com.typesafe.config.ConfigFactory
 import spray.routing._
 import spray.routing.authentication.BasicAuth
 
@@ -11,13 +12,15 @@ import com.ovoenergy.ovoroo.services.OrderService
 
 class AppInterface(implicit val executionContext: ExecutionContext) extends HttpServiceActor with CORSSupport with UserResource {
 
+  val config = ConfigFactory.load()
+
   def receive = runRoute(routes)
 
   val orderService = new OrderService
 
   val routes: Route =
     authenticate(BasicAuth(ldapAuthenticator _, realm = "Use for OVO windows credentials")) { ldapUser =>
-        pathEnd { complete(204, None) }
+        pathEnd { getFromDirectory(config.getString("app.directory")) }
     }
 
 }
